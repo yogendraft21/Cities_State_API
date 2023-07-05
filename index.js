f248db053736ede51887248ffd083696a3eb1854
yogendraft21 <yogendra311204@gmail.com>
2023-07-05 12:07:12 +0530

    cities updated

--- index.js
+++ index.js
@@ -25,6 +25,39 @@ app.get("/yogi/api/india/state", (req, res) => {
   });
 });
 
+
+app.get("/yogi/api/india/state/:stateName", (req, res) => {
+
+    fs.readFile("data.json", "utf8", (err, data) => {
+      if (err) {
+        console.error("Error reading file:", err);
+        res.status(500).send("Error reading file");
+        return;
+      }
+  
+      try {
+        const jsonData = JSON.parse(data);
+  
+        const state = jsonData.states.find(
+          (state) => state.state === req.params.stateName
+        );
+  
+        if (!state) {
+          res.status(404).send("State not found");
+          return;
+        }
+  
+        const districts = state.districts;
+  
+        res.json(districts);
+      } catch (error) {
+        console.error("Error parsing JSON:", error);
+        res.status(500).send("Error parsing JSON");
+      }
+    });
+  });
+
+
 app.listen(8000, () => {
   console.log("Server running on port 8000");
 });
